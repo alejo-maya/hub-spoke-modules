12d00a44ad2821f2dc030c1454918190934677f1
alejo-maya <alejo.maya@southworks.com>
2024-09-05 10:16:50 -0300

    deleted commented code

--- hub-spoke-modules/aks/dns.tf
+++ hub-spoke-modules/aks/dns.tf
@@ -1,8 +1,3 @@
-# resource "azurerm_private_dns_zone" "aks_private_zone" {
-#   name                = var.dns.private_dns_zone_name
-#   resource_group_name = var.external.resource_group_name
-# }
-
 module "dns_zone" {
   source = "../dns_zone"
 
@@ -33,21 +28,3 @@ module "aks_dns_link_hub" {
     virtual_network_id    = var.external.hub_vnet_id
   }
 }
-
-# resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_link_spoke" {
-#   name                  = var.dns.dns_link_spoke_name
-#   resource_group_name   = var.external.resource_group_name
-#   private_dns_zone_name = azurerm_private_dns_zone.aks_private_zone.name
-#   virtual_network_id    = azurerm_virtual_network.aks_spoke_vnet.id
-
-#   depends_on = [azurerm_private_dns_zone.aks_private_zone]
-# }
-
-# resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_link_hub" {
-#   name                  = var.dns.dns_link_hub_name
-#   resource_group_name   = var.external.resource_group_name
-#   private_dns_zone_name = azurerm_private_dns_zone.aks_private_zone.name
-#   virtual_network_id    = var.external.hub_vnet_id
-
-#   depends_on = [azurerm_private_dns_zone.aks_private_zone]
-# }
--- hub-spoke-modules/aks/route_table.tf
+++ hub-spoke-modules/aks/route_table.tf
@@ -29,23 +29,3 @@ module "route_table_association" {
     route_table_id = module.route_table.route_table_id
   }
 }
-
-# route_table.tf
-# resource "azurerm_route_table" "aks_route_table" {
-#   name                = var.route_table.route_table_name
-#   location            = var.external.location
-#   resource_group_name = var.external.resource_group_name
-
-#   route {
-#     name                   = var.route_table.route_name
-#     address_prefix         = var.route_table.address_prefix
-#     next_hop_type          = var.route_table.next_hop_type
-#     next_hop_in_ip_address = var.external.next_hop_ip_address
-#   }
-# }
-
-# resource "azurerm_subnet_route_table_association" "aks_subnet_association" {
-#   subnet_id      = azurerm_subnet.aks_spoke_subnet.id
-#   route_table_id = azurerm_route_table.aks_route_table.id
-# }
-
